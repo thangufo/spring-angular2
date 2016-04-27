abab60aa294b4212da4c56461fa78cf3284ed742
Thang Nguyen <thang.nguyen@luxbet.com>
2016-04-27 10:41:58 +1000

    Initial commit

---
+++ src/main/java/com/thang/Angular2Application.java
@@ -0,0 +1,12 @@
+package com.thang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Angular2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Angular2Application.class, args);
+	}
+}
