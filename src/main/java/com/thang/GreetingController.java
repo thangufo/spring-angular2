b26a6bb857991a36e8e0676ad2248addf9599035
Thang Nguyen <thang.nguyen@luxbet.com>
2016-04-27 15:54:02 +1000

    Remove build file

--- src/main/java/com/thang/GreetingController.java
+++ src/main/java/com/thang/GreetingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello 123, %s!";
+    private static final String template = "Hello 1234, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
