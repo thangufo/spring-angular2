df25455aea9a407a04640c607d89022d65d0a01b
ThangNguyen <thangnguyen@thangnguyenmbp.home>
2016-04-28 00:08:55 +1000

    Update index.html

--- src/main/resources/public/app/app.ts
+++ src/main/resources/public/app/app.ts
@@ -1,7 +1,7 @@
 ///<reference path="node_modules/angular2/ts/typings/node/node.d.ts"/>
 ///<reference path="node_modules/angular2/typings/browser.d.ts"/>
-import { bootstrap } from "angular2/platform/browser";
-import { Component } from "angular2/core";
+import {bootstrap} from "angular2/platform/browser";
+import {Component} from "angular2/core";
 
 @Component({
     selector:'hello-world',
